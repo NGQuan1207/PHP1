2c95ad99293bbb3ff376e302dc1653cf3e77029c
Quan <minhquan12072002@gmail.com>
2026-01-15 10:37:00 +0700

    lab 2 minor

--- Lab/lab2/classes.php
+++ Lab/lab2/classes.php
@@ -5,29 +5,24 @@ class Person {
     public $age;
     public $address;
 
-    // Phương thức thiết lập tên
     public function setName($name) {
         $this->name = $name;
     }
 
-    // Phương thức thiết lập tuổi
     public function setAge($age) {
         $this->age = $age;
     }
 
-    // Phương thức thiết lập địa chỉ
     public function setAddress($address) {
         $this->address = $address;
     }
 
-    // Phương thức lấy thông tin đầy đủ
     public function getInfo() {
         return "Tên: " . $this->name . "<br>" .
                "Tuổi: " . $this->age . "<br>" .
                "Địa chỉ: " . $this->address;
     }
 
-    // Phương thức kiểm tra có thể bỏ phiếu không
     public function canVote() {
         if ($this->age >= 18) {
             return true;
@@ -43,29 +38,24 @@ class Product {
     public $price;
     public $quantity;
 
-    // Phương thức thiết lập tên sản phẩm
     public function setName($name) {
         $this->name = $name;
     }
 
-    // Phương thức thiết lập giá
     public function setPrice($price) {
         $this->price = $price;
     }
 
-    // Phương thức thiết lập số lượng
     public function setQuantity($quantity) {
         $this->quantity = $quantity;
     }
 
-    // Phương thức lấy thông tin sản phẩm
     public function getInfo() {
         return "Tên sản phẩm: " . $this->name . "<br>" .
                "Giá: " . number_format($this->price) . " VNĐ<br>" .
                "Số lượng: " . $this->quantity;
     }
 
-    // Phương thức tính tổng giá trị
     public function calculateTotal() {
         return $this->price * $this->quantity;
     }
@@ -79,17 +69,7 @@ class SinhVien {
     private $ngaysinh;
     private $diemtb;
 
-    // Constructor mặc định
-    public function __construct() {
-        $this->mssv = "";
-        $this->hoten = "";
-        $this->gioitinh = "";
-        $this->ngaysinh = "";
-        $this->diemtb = 0;
-    }
-
-    // Constructor có tham số
-    public function __construct_full($mssv, $hoten, $gioitinh, $ngaysinh, $diemtb) {
+    public function __construct($mssv = "", $hoten = "", $gioitinh = "", $ngaysinh = "", $diemtb = 0) {
         $this->mssv = $mssv;
         $this->hoten = $hoten;
         $this->gioitinh = $gioitinh;
@@ -97,7 +77,6 @@ class SinhVien {
         $this->diemtb = $diemtb;
     }
 
-    // Getter và Setter cho MSSV
     public function getMssv() {
         return $this->mssv;
     }
@@ -106,7 +85,6 @@ class SinhVien {
         $this->mssv = $mssv;
     }
 
-    // Getter và Setter cho Họ tên
     public function getHoten() {
         return $this->hoten;
     }
@@ -115,7 +93,6 @@ class SinhVien {
         $this->hoten = $hoten;
     }
 
-    // Getter và Setter cho Giới tính
     public function getGioitinh() {
         return $this->gioitinh;
     }
@@ -124,7 +101,6 @@ class SinhVien {
         $this->gioitinh = $gioitinh;
     }
 
-    // Getter và Setter cho Ngày sinh
     public function getNgaysinh() {
         return $this->ngaysinh;
     }
@@ -133,7 +109,6 @@ class SinhVien {
         $this->ngaysinh = $ngaysinh;
     }
 
-    // Getter và Setter cho Điểm TB
     public function getDiemtb() {
         return $this->diemtb;
     }
@@ -142,36 +117,12 @@ class SinhVien {
         $this->diemtb = $diemtb;
     }
 
-    // Phương thức hiển thị thông tin sinh viên
     public function hienThiThongTin() {
         echo "MSSV: " . $this->mssv . "<br>";
         echo "Họ tên: " . $this->hoten . "<br>";
         echo "Giới tính: " . $this->gioitinh . "<br>";
         echo "Ngày sinh: " . $this->ngaysinh . "<br>";
-        echo "Điểm TB: " . $this->diemtb . "<br>";
-        echo "<hr>";
-    }
-}
-
-// Khởi tạo mảng sinh viên
-$mangSinhVien = array();
-
-// Hàm thêm sinh viên vào mảng
-function themSinhVien($sv) {
-    global $mangSinhVien;
-    $mangSinhVien[] = $sv;
-}
-
-// Hàm hiển thị tất cả sinh viên
-function hienThiTatCaSinhVien() {
-    global $mangSinhVien;
-    if (count($mangSinhVien) > 0) {
-        echo "<h3>Danh sách sinh viên:</h3>";
-        foreach ($mangSinhVien as $sv) {
-            $sv->hienThiThongTin();
-        }
-    } else {
-        echo "<p>Chưa có sinh viên nào!</p>";
+        echo "Điểm TB: " . $this->diemtb . "<br><hr>";
     }
 }
 ?>
